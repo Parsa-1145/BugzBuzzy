54ed963b81b189e83d08ca69f4e438e448f0a860
Parsa <parsazamirikisomi@gmail.com>
2025-10-30 15:32:50 +0330

    made a scene for skeleton

--- Tower.gd
+++ Tower.gd
@@ -1,6 +1,21 @@
 class_name Tower
 extends StaticBody3D
 
+@onready var head : Node3D = $Head
 
 func _process(delta: float) -> void:
+	var minDist: float = INF
+	var closestEnemy: EnemyBase
+	for enemy: EnemyBase in GameManager.enemies:
+		var dist: float = (enemy.position - position).length()
+		if  dist < minDist:
+			minDist = dist
+			closestEnemy = enemy
+	
+	if closestEnemy == null:
+		return
+	
+	#var headDiff = closestEnemy.position - head.position
+	head.look_at(closestEnemy.position)
+	head.rotation.x = 90
 	
